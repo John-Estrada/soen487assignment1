4eb5f40f8e7fbf2e28f9446e98f05d47efad824c
John-Estrada <jestrada740@gmail.com>
2021-02-09 17:59:09 -0500

    core

---
+++ src/main/java/com/example/core/Album.java
@@ -0,0 +1,68 @@
+package com.example.core;
+
+import java.util.ArrayList;
+
+public class Album {
+    private int isrc;
+    private String title;
+    private String description;
+    private int year;
+    private Artist artist;
+
+    private static int currentIsrc=0;
+
+    public Album(String title, String description, int year, Artist artist) {
+        this.isrc = this.currentIsrc++;
+        this.title = title;
+        this.description = description;
+        this.year = year;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public int getIsrc() {
+        return isrc;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "isrc=" + isrc +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", year=" + year +
+                ", artist=" + artist +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/core/Artist.java
@@ -0,0 +1,74 @@
+package com.example.core;
+
+import java.util.ArrayList;
+
+public class Artist {
+    private String nickName;
+    private String firstName;
+    private String lastName;
+    private String bio;
+
+    private static ArrayList<String> usedNickNames = new ArrayList<String>();
+    private static ArrayList<Artist> allArtists = new ArrayList<Artist>();
+
+    public Artist(String nickName, String firstName, String lastName, String bio) throws Exception {
+        if (!this.usedNickNames.contains(nickName)) {
+            this.nickName = nickName;
+            this.usedNickNames.add(nickName);
+        } else {
+            throw new Exception("This nickname is already in use.");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bio = bio;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        if (!this.usedNickNames.contains(nickName)) {
+            String prevName = this.nickName;
+            this.nickName = nickName;
+            this.usedNickNames.add(nickName);
+            usedNickNames.remove(prevName);
+        } else {
+            System.out.println("This nickname is already in use.");
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "nickName='" + nickName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", bio='" + bio + '\'' +
+                '}';
+    }
+}
